159291a638b51d7b54984c500ea52d02b623b6eb
Yash12patre <0126cs201128@oriental.ac.in>
2025-09-12 20:52:29 +0530

    controller setup

--- backend/src/controllers/aiController.js
+++ backend/src/controllers/aiController.js
@@ -1,3 +1,11 @@
+import OpenAI from "openai"
+import sql from "../db.js"
+const AI =new OpenAI(
+    api_key=process.env.GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
 export const generateArticle =async (req,res)=>{
     try {
         const {userId}=req.auth();
@@ -5,8 +13,25 @@ export const generateArticle =async (req,res)=>{
         const plan =req.plan;
         const free_usage=req.free_usage;
         if(plan != "premium" && free_usage >=10){
-            return req.json()
+            return req.json({success: false, message:"Limit Reached. Upgrade to continue"})
         }
+        response = await AI.chat.completions.create({
+
+            model:"gemini-2.5-flash",
+            messages:[{
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        temprature:0.7,
+        max_token:length,
+    });
+    const content =response.choices[0].message.content
+    await sql`INSERT INTO creations(user_id, prompt, content, type)
+              VALUE( ${userId},  ${prompt}, ${content}, 'article')`;
+              if(plan !== "premium"){
+                await clerkClient
+              }
     } catch (error) {
         
     }
